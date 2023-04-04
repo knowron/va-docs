f661344a56965d65ac503d6860b4648accf864f4
Arturo Buitrago Méndez <arturo@knowron.com>
2023-04-04 11:16:55 +0200

    Update sync file

--- scripts/sync.py
+++ scripts/sync.py
@@ -3,7 +3,7 @@ import shutil
 import os
 
 def sync():
-    original_path = "../../docs/docs"
+    original_path = "../../knowron-docs/docs"
     destination_path ="../docs"
     if os.path.isdir(original_path) and os.path.isdir(destination_path):
         shutil.copytree(original_path, destination_path, dirs_exist_ok=True)
